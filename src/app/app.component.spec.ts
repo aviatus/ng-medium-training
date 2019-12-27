06595b15fee4006a8bf9929be1707e372ae31ed9
Ahmet Yildiz <ahmet.yildiz@ottoo.com>
2019-12-27 17:55:22 +0300

    Add app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,14 @@
-import { async, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { async, inject, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture;
+  let app;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -12,12 +17,24 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      app = fixture.debugElement.componentInstance;
+    });
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('Customer List return button click check router', async(inject([Location],
+    (location: Location) => {
+      fixture.detectChanges();
+      const button = fixture.debugElement.query(By.css('a.navbar-brand'));
+      button.nativeElement.click();
+      fixture.whenStable().then(() => {
+        expect(location.path()).toBe('/');
+      });
+    })
+  ));
 });
